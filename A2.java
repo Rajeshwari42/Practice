e6a2e4eee3f0a52553ce0028c62adec95a9690be
Rajeshwari42 <144303866+Rajeshwari42@users.noreply.github.com>
2023-11-28 14:36:58 +0530

    Add files via upload

---
+++ A2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class A2 {
+	 public static void main (String args[]) {
+		
+		 Scanner scan=new Scanner(System.in);
+		 {
+			 int a=scan.nextInt();
+			 System.out.println("Enter a first number:");
+			 int b=scan.nextInt();
+			 System.out.println("Enter a second number:");
+			 System.out.println("Enter a first number:" + a);
+			 System.out.println("Enter a second number:" + b);
+			 
+		 }
+	 }
+}
+
+
